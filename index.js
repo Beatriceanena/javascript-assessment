91e0e116c9f714cc04cf8e03bd4320782a0912ec
Beatriceanena <anenabeattrice@gmail.com>
2022-08-09 18:10:26 +0300

    changes

---
+++ index.js
@@ -0,0 +1,10 @@
+var phonenumber = document.getElementById("phonenumber")
+phonenumber.appendChild(+256789035587)
+
+var names = document.getElementById("name")
+names.appendChild('suzan')
+
+var locate = document.getElementById("location")
+location.appendChild("karen")
+  
+
