ead8794dc2029b67a16e47bd7377445d8c762b40
Beatriceanena <anenabeattrice@gmail.com>
2022-08-09 18:15:43 +0300

    chnages

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var phonenumber = document.getElementById("phonenumber")
+var phonenumber = document.getElementById("number")
 phonenumber.appendChild(+256789035587)
 
-var names = document.getElementById("name")
+var names = document.getElementById("company")
 names.appendChild('suzan')
 
-var locate = document.getElementById("location")
+var locate = document.getElementById("text")
 location.appendChild("karen")
   
 
